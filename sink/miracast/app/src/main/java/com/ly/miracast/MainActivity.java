7a1281ebaec1b81c93d4c5a6630162992a90562f
longmaoaiali <13213102491@163.com>
2021-01-23 12:20:49 +0800

    add spinner control

--- sink/miracast/app/src/main/java/com/ly/miracast/MainActivity.java
+++ sink/miracast/app/src/main/java/com/ly/miracast/MainActivity.java
@@ -3,17 +3,29 @@ package com.ly.miracast;
 
 import android.Manifest;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.UserHandle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    //todo:maybe more permission
     private static String[] smPermissions = new String[]{
             Manifest.permission.INTERNET,
             Manifest.permission.CHANGE_WIFI_STATE,
@@ -35,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.CHANGE_NETWORK_STATE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WAKE_LOCK,
     };
     private static int smRequestPermissionCode = 644;
 
@@ -46,6 +60,10 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mSharedPreferencesEditor;
 
+    private PowerManager.WakeLock mWakeLock;
+    private boolean mFirstInit = false;
+
+    public boolean mStartConnecting = false;
 
     private void requestPermissions(){
         requestPermissions(smPermissions,smRequestPermissionCode);
@@ -103,6 +121,102 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         //todo:onResume
         Log.d(TAG,"onResume");
+        /*Remove the current p2p group*/
+        mWifiP2pManager.removeGroup(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.d(TAG,"removeGroup Success");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Log.d(TAG,"removeGroup failed");
+            }
+        });
+
+        //enable backlight
+        PowerManager powerManager = (PowerManager)getSystemService(Context.POWER_SERVICE);
+        mWakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE,TAG);
+        mWakeLock.acquire();
+        if(mFirstInit){
+            //第二次运行时需要remove 滞留广播
+            Intent intent = new Intent(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+            removeStickyBroadcast(intent);
+        } else {
+            mFirstInit = true;
+        }
+        //miracast certification todo:
+        initCert();
+
+
+    }
+
+    private ArrayList<String> mDeviceList;
+    private Spinner mSpinner, mSpinDevice;
+    private InputMethodManager mInputMethodManager;
+    private void initCert() {
+        //todo:
+        initFunList();
+        mStartConnecting = false;
+        mDeviceList = new ArrayList<String>();
+        mDeviceList.add(" ");
+
+        mSpinner = (Spinner)findViewById(R.id.spinner1);
+        mInputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,mFunList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
+        mSpinner.setPrompt("Please function！");
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 1: //start scan
+                        Log.d(TAG,"mSpinner View select"+position);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
+
+    private ArrayList<String> mFunList;
+    private void initFunList() {
+        mFunList = new ArrayList<String>();
+        mFunList.add(" ");
+        mFunList.add(" ");
+        mFunList.add("Start scan");
+        mFunList.add("Stop scan");
+        mFunList.add("Start listen");
+        mFunList.add("Stop listen");
+        mFunList.add("Enable autonomous GO");
+        mFunList.add("Disable autonomous GO");
+        mFunList.add("Connect to peer by display_pin");
+        mFunList.add("Connect to peer by push_button");
+        mFunList.add("Connect to peer by enter_pin");
+        mFunList.add("Set wfd session manual mode");
+        mFunList.add("Set wfd session auto mode");
+        mFunList.add("Continue remaining wfd session under manual mode");
+        mFunList.add("Enable wfd");
+        mFunList.add("Disable wfd");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause()");
+        //todo
+        mWakeLock.release();
     }
 
     /**
